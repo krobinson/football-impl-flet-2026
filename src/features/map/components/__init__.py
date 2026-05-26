e72940feb2cb237b67c2398f1994568beb13047b
Ken Robinson <kenrobinsonster@gmail.com>
2026-05-26 22:25:34 +1000

    moved view file to view directory

--- src/features/map/components/__init__.py
+++ src/features/map/components/__init__.py
@@ -1,6 +1,6 @@
 """Public surface of features.map.components."""
 from features.map.components.group_filter import GroupFilter
 from features.map.components.info_panel import InfoPanel
-from features.map.components.map_view import MapView
+from features.map.views.map_view import MapView  # re-exported for backward compat
 
 __all__ = ["GroupFilter", "InfoPanel", "MapView"]
---
+++ src/features/map/views/__init__.py
@@ -0,0 +1,4 @@
+"""Public surface of features.map.views."""
+from features.map.views.map_view import MapView
+
+__all__ = ["MapView"]
